5d75acd50f75f96f6dd9fa7493a48ee4f5c4b1ff
LongTN <longtn2@rikkeisoft.com>
2024-05-10 17:29:59 +0700

    add bunit

---
+++ 8.0/bUnitTest/CounterCSharpTests.cs
@@ -0,0 +1,19 @@
+using Bunit;
+using BlazorSample.Layout;
+using Xunit;
+namespace bUnitTest
+{
+
+    public class CounterCSharpTests : TestContext
+    {
+        [Fact]
+        public void CounterStartsAtZero()
+        {
+            // Arrange
+            var cut = RenderComponent<DoctorWhoLayout>();
+
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("header").MarkupMatches("<header><h1>Doctor Who&reg; Database</h1></header>");
+        }
+    }
+}
